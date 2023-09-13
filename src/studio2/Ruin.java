02044d2f5e8fbea834aada969a49d439f16646e6
danielpalmer <danielpalmer@localhost>
2023-09-13 14:14:00 -0500

    completed ish 9/13

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -8,40 +8,65 @@ public class Ruin {
 		// TODO Auto-generated method stub
 
 		Scanner in = new Scanner(System.in);
-		
+
 		System.out.println("Start Amount: ");
-		int startAmount = in.nextInt();
+		int firstStartAmount = in.nextInt();
 		System.out.println("Win Chance: ");
 		int winChance = in.nextInt();
 		System.out.println("Win Limit: ");
 		int winLimit = in.nextInt();
 		System.out.println("Loss Limit: ");
 		int lossLimit = in.nextInt();
-		
-		
-		
-		while (winLimit > startAmount && startAmount > lossLimit)
+		System.out.println("Total Simulations: ");
+		int totalSimulations = in.nextInt();
+
+		int numLosses = 0;
+
+		for ( int count = 1; count <= totalSimulations; count++)
 		{
-			int random = (int) (Math.random()*100-0);
-			if(random < winChance)
+			int numPlays = 0;
+			int startAmount = firstStartAmount;
+			while ((winLimit > startAmount) && (startAmount > lossLimit))
 			{
-				startAmount ++;
+				int random = (int) (Math.random()*100-0);
+				if(random < winChance)
+				{
+					startAmount ++;
+				}
+				else
+				{
+					startAmount --;
+				}
+				numPlays++;
+			}
+			
+			System.out.println("Day " + count);
+			System.out.println("Number of plays: " + numPlays);
+			if (startAmount > lossLimit)
+			{
+				System.out.println("Win");
 			}
 			else
 			{
-				startAmount --;
+				System.out.println("Loss");
+				numLosses++;
 			}
+			
 		}
 		
-		if (startAmount > lossLimit)
-		{
-			System.out.println("Win");
-		}
-		else
-		{
-			System.out.println("Loss");
-		}
+		System.out.println("Number of losses: " + numLosses);
+		System.out.println("Simulations: " + totalSimulations);
+		double ruinRate =  ((numLosses*1.0)/totalSimulations);
+		System.out.println("Ruin rate from Simulation: " + (ruinRate));
 		
+		double expectedRuin = ((1 - (winChance*1.0))/winChance);
+		expectedRuin = (((Math.pow(expectedRuin, (firstStartAmount*1.0)))-(Math.pow(expectedRuin, (winLimit*1.0))))/(1-(Math.pow(expectedRuin, (winLimit*1.0)))));
+		
+		System.out.println("Expected ruin rate: " + expectedRuin);
+		
+
+		
+
 	}
 
 }
